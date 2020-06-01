395c6bb359bd08941639daa5f798786f93719fa4
Alexandr <alerusford@gmail.com>
2020-06-01 22:25:13 +0300

    Initial commit

---
+++ src/ru/alerusford/javabasic/Test.java
@@ -0,0 +1,13 @@
+package ru.alerusford.javabasic;
+
+public class Test {
+    public static void main(String[] args) {
+       int a = 4;
+       int b = 4;
+       //boolean result;
+
+
+        if (a == b) System.out.println("Yeah!");
+        else System.out.println("Ohhh...");
+    }
+}
